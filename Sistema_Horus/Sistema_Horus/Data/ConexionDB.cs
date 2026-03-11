42dd2f15a0abbfd3cc8f051dc894f79edf092aa3
SixtoMV31 <sixtomendezvelazquez58@gmil.com>
2026-03-11 11:04:18 -0600

    elemento modificado para prueba en administracion

--- Sistema_Horus/Sistema_Horus/Data/ConexionDB.cs
+++ Sistema_Horus/Sistema_Horus/Data/ConexionDB.cs
@@ -10,6 +10,7 @@ namespace Sistema_Horus.Data
 {
     public class ConexionDB
     {
+        // Esta es otra prueba xd 
         private string conexion= "Server=MENDEZ\\SQLEXPRESS;Database=BD_Horus;Trusted_Connection=True;TrustServerCertificate=True;";
         public SqlConnection ObtenerConexion()
         {
